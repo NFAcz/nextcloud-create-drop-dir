f37170b207345d57171f1ba49535d35a914eb2dc
Jonas Svatos <jonas.svatos@nfa.cz>
2017-10-30 17:18:50 +0100

    add random uuid

--- nextcloud-create-drop-dir.py
+++ nextcloud-create-drop-dir.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import owncloud
 import unidecode
+import uuid
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,12 +13,12 @@ parser.add_argument('-n', '--name', help='Name of the directory', required=True)
 parser.add_argument('-d', '--dir', help='Path where to create a shared directory', default='DIGILAB/DROP_BOX')
 args = parser.parse_args()
 
-name = unidecode.unidecode(args.name.decode('utf8')).strip().replace(' ','_').upper()
+name = unidecode.unidecode(args.name.decode('utf8')).strip().replace(' ', '_').upper()
+dst_dir = '{0}/{1}_{2}'.format(args.dir, name, uuid.uuid4().hex)
 oc = owncloud.Client('https://{0}'.format(args.server))
 oc.login(args.user, args.password)
-oc.mkdir('{0}/{1}'.format(args.dir, name))
-link_info = oc.share_file_with_link('{0}/{1}'.format(args.dir, name))
+oc.mkdir(dst_dir)
+link_info = oc.share_file_with_link(dst_dir)
 oc.logout()
 
 print('{0} : {1}'.format(name, link_info.get_link()), end='')
-
